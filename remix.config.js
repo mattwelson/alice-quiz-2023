fa926a0c3f53bfbe64cc5b14dbb9f0b96b8c7165
mattwelson <matthew.welson@gmail.com>
2024-01-01 15:02:33 +1300

    chore: more netlify changes

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,11 @@
+import { config } from "@netlify/remix-adapter"
+
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  ...config,
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
-};
+}
