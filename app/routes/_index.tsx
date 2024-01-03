296d3f89b0ff3016aa68cfb42751d179a5e3570e
mattwelson <matthew.welson@gmail.com>
2024-01-03 20:25:19 +1300

    fix: hiker -> tramper

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,22 +1,22 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/node"
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Hiker Type - Alice Adventuring" },
-    { name: "description", content: "What type of hiker are you?" },
-  ];
-};
+    { title: "Tramper Type - Alice Adventuring" },
+    { name: "description", content: "What type of tramper are you?" },
+  ]
+}
 
 export default function Index() {
   return (
-    <div className="prose">
-      <h1>Hiker Type</h1>
-      <p>What type of hiker are you?</p>
+    <div className='prose'>
+      <h1>Tramper Type</h1>
+      <p>What type of tramper are you?</p>
       <p>
-        <a href="/quiz" className="btn btn-primary">
+        <a href='/quiz' className='btn btn-primary'>
           Take the quiz!
         </a>
       </p>
     </div>
-  );
+  )
 }
